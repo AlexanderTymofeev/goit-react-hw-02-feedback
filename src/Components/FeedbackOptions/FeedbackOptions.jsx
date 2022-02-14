20f7c02a981ecd0df05de304d1fcdfaeec390915
AlexanderTymofeev <timofeev.mobile@gmail.com>
2022-02-14 19:40:51 +0200

    Добавляет ДЗ#2 Feedback

---
+++ src/Components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// import nanoid from 'nanoid';
+import PropTypes from 'prop-types';
+import styles from "./FeedbackOptions.module.css";
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+	return (
+		<>
+			{options.map((option) => (
+				<button className={styles.btn} type="button" name={option} onClick={onLeaveFeedback}>
+					{option}
+				</button>
+			))}
+		</>
+	);
+};
+FeedbackOptions.propTypes = {
+	options: PropTypes.array.isRequired,
+	onLeaveFeedback: PropTypes.func.isRequired
+};
+
+export default FeedbackOptions;
